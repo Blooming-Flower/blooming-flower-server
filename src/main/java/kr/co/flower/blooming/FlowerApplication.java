ddba5c396eb8006a555bb4d88905c02b07e5cd85
shmin7777 <shmin7777@naver.com>
2023-08-08 23:27:19 +0900

    pdf generate sample code 추가

--- src/main/java/kr/co/flower/blooming/FlowerApplication.java
+++ src/main/java/kr/co/flower/blooming/FlowerApplication.java
@@ -1,13 +1,29 @@
 package kr.co.flower.blooming;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FlowerApplication {
+import kr.co.flower.blooming.pdf.PdfGenerator;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@SpringBootApplication
+public class FlowerApplication implements ApplicationRunner{
+
+	@Autowired
+	private PdfGenerator pdfGenerator;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FlowerApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("generate:!!!!!");
+		pdfGenerator.generate();
 	}
 
 }
