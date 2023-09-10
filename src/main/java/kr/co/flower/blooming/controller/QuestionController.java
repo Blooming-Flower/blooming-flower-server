c36b1db847c1a7b2c877f95f1ae8f2bfafe86d1a
shmin7777 <shmin7777@naver.com>
2023-09-10 23:20:56 +0900

    지문 수정 버튼 - 지문, 문제 유형 list 조회 controller 추가

--- src/main/java/kr/co/flower/blooming/controller/QuestionController.java
+++ src/main/java/kr/co/flower/blooming/controller/QuestionController.java
@@ -22,33 +22,36 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class QuestionController {
-    private final QuestionService questionService;
+	private final QuestionService questionService;
 
-    @Operation(description = "문제 저장", summary = "문제 저장")
-    @PostMapping(path = "/save")
-    public ResponseEntity<?> saveQuestion(@RequestBody @Valid QuestionRegistDto questionRegistDto) {
-        questionService.saveQuestion(questionRegistDto);
-        return ResponseEntity.ok().build();
-    }
+	@Operation(description = "문제 저장", summary = "문제 저장")
+	@PostMapping(path = "/save")
+	public ResponseEntity<?> saveQuestion(@RequestBody @Valid QuestionRegistDto questionRegistDto) {
+		questionService.saveQuestion(questionRegistDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @ApiResponses({
-            @ApiResponse(responseCode = "404", description = "문제를 찾을 수 없습니다.")
-    })
-    @Operation(description = "문제 수정", summary = "문제 수정")
-    @PutMapping(path = "/update")
-    public ResponseEntity<?> updateQuestion(
-            @RequestBody @Valid QuestionRegistDto questionRegistDto) {
-        questionService.updateQuestion(questionRegistDto);
-        return ResponseEntity.ok().build();
-    }
+	@ApiResponses({ @ApiResponse(responseCode = "404", description = "문제를 찾을 수 없습니다.") })
+	@Operation(description = "문제 수정", summary = "문제 수정")
+	@PutMapping(path = "/update")
+	public ResponseEntity<?> updateQuestion(@RequestBody @Valid QuestionRegistDto questionRegistDto) {
+		questionService.updateQuestion(questionRegistDto);
+		return ResponseEntity.ok().build();
+	}
 
-    @ApiResponses({
-            @ApiResponse(responseCode = "404", description = "지문을 찾을 수 없습니다.")
-    })
-    @Operation(description = "문제 삭제", summary = "문제 삭제")
-    @DeleteMapping(path = "/delete/{questionId}")
-    public ResponseEntity<?> deletePassage(@PathVariable(name = "questionId") long questionId) {
-        questionService.deleteQuestion(questionId);
-        return ResponseEntity.ok().build();
-    }
+	@ApiResponses({ @ApiResponse(responseCode = "404", description = "지문을 찾을 수 없습니다.") })
+	@Operation(description = "문제 삭제", summary = "문제 삭제")
+	@DeleteMapping(path = "/delete/{questionId}")
+	public ResponseEntity<?> deleteQuestion(@PathVariable(name = "questionId") long questionId) {
+		questionService.deleteQuestion(questionId);
+		return ResponseEntity.ok().build();
+	}
+
+	@ApiResponses({ @ApiResponse(responseCode = "404", description = "지문을 찾을 수 없습니다.") })
+	@Operation(description = "지문 수정 버튼 누를 시 화면 목록 조회", summary = "지문 수정 버튼 - 지문, 문제 유형 list 조회")
+	@DeleteMapping(path = "/search/{questionId}")
+	public ResponseEntity<?> searchQuestionList(@PathVariable(name = "questionId") long questionId) {
+		questionService.deleteQuestion(questionId);
+		return ResponseEntity.ok().build();
+	}
 }
