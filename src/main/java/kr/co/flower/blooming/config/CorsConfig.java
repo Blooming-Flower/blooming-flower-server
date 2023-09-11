21acdf538018a4350c903194093ecc01a4147284
Min Hong <shmin7777@naver.com>
2023-09-11 13:40:09 +0900

    cors config 추가

---
+++ src/main/java/kr/co/flower/blooming/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package kr.co.flower.blooming.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS configuration
+ * 
+ * @author shmin
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // TODO 추후 프론트 서버로 MAPPING 정보 바꿔줌
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+}
