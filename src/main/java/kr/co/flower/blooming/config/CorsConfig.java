33c100949594ddfb870ec902cfb2353a0e67ff68
shmin7777 <shmin7777@naver.com>
2023-09-12 21:05:59 +0900

    cors config 수정

--- src/main/java/kr/co/flower/blooming/config/CorsConfig.java
+++ src/main/java/kr/co/flower/blooming/config/CorsConfig.java
@@ -14,13 +14,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(
-                "http://43.201.142.170:3000, http://localhost:3000, http://127.0.0.1:3000, http://localhost:5000, http://127.0.0.1:5000")
-                .allowedHeaders("Accept", "Origin", "X-Requested-With", "Content-Type",
-                        "Access-Control-Allow-Origin", "Access-Control-Allow-Headers")
-                .allowedMethods("*")
-                .exposedHeaders("Content-Disposition");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOriginPatterns("*")
+		.allowedHeaders("Accept", "Origin", "X-Requested-With",
+				"Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers")
+		.allowedMethods("*")
+				.exposedHeaders("Content-Disposition");
+	}
 }
