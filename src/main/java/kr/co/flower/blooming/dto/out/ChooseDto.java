0a180ef6f3277c4274af4f8c52a5ff5e77775778
shmin7777 <shmin7777@naver.com>
2023-09-22 00:37:40 +0900

    [문제 수정] 수정버튼 누르면 나오는 api 수정

---
+++ src/main/java/kr/co/flower/blooming/dto/out/ChooseDto.java
@@ -0,0 +1,17 @@
+package kr.co.flower.blooming.dto.out;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 선지 dto
+ * 
+ * @author shmin
+ *
+ */
+@Getter
+@Setter
+public class ChooseDto {
+	private int seq;
+	private String content;
+}
--- src/main/java/kr/co/flower/blooming/service/PassageService.java
+++ src/main/java/kr/co/flower/blooming/service/PassageService.java
@@ -1,7 +1,9 @@
 package kr.co.flower.blooming.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
@@ -11,16 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.flower.blooming.dto.in.PassageRegistParam;
 import kr.co.flower.blooming.dto.out.CheckExistPassageDto;
+import kr.co.flower.blooming.dto.out.ChooseDto;
 import kr.co.flower.blooming.dto.out.PassageListDto;
 import kr.co.flower.blooming.dto.out.PassageNameDto;
 import kr.co.flower.blooming.dto.out.SearchPassageDto;
-import kr.co.flower.blooming.dto.out.SearchPassageDto.SearchQuestionDto;
+import kr.co.flower.blooming.dto.out.SearchPassageDto.SearchQuestionDtos;
+import kr.co.flower.blooming.dto.out.SearchPassageDto.SearchQuestionDtos.SearchQuestionDto;
+import kr.co.flower.blooming.entity.AnswerDto;
 import kr.co.flower.blooming.entity.PassageEntity;
 import kr.co.flower.blooming.entity.PassageType;
 import kr.co.flower.blooming.entity.QuestionEntity;
 import kr.co.flower.blooming.exception.FlowerError;
 import kr.co.flower.blooming.exception.FlowerException;
 import kr.co.flower.blooming.repository.PassageRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,116 +41,143 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PassageService {
-    private final PassageRepository passageRepository;
+	private final PassageRepository passageRepository;
 
-    /**
-     * 지문 저장
-     * 
-     * @param passageRegistDto
-     */
-    @Transactional
-    public void savePassage(PassageRegistParam passageRegistParam) {
-        PassageEntity passageEntity = new PassageEntity();
+	/**
+	 * 지문 저장
+	 * 
+	 * @param passageRegistDto
+	 */
+	@Transactional
+	public void savePassage(PassageRegistParam passageRegistParam) {
+		PassageEntity passageEntity = new PassageEntity();
 
-        setPassageEntity(passageEntity, passageRegistParam);
+		setPassageEntity(passageEntity, passageRegistParam);
 
-        passageRepository.save(passageEntity);
-    }
+		passageRepository.save(passageEntity);
+	}
 
-    /**
-     * 지문 수정
-     * 
-     * @param passageRegistDto
-     */
-    @Transactional
-    public void updatePassage(PassageRegistParam passageRegistParam) {
-        PassageEntity passageEntity = passageRepository.findById(passageRegistParam.getPassageId())
-                .orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
+	/**
+	 * 지문 수정
+	 * 
+	 * @param passageRegistDto
+	 */
+	@Transactional
+	public void updatePassage(PassageRegistParam passageRegistParam) {
+		PassageEntity passageEntity = passageRepository.findById(passageRegistParam.getPassageId())
+				.orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
 
-        setPassageEntity(passageEntity, passageRegistParam);
-    }
+		setPassageEntity(passageEntity, passageRegistParam);
+	}
 
-    /**
-     * 지문 삭제
-     * 
-     * @param passageId
-     */
-    @Transactional
-    public void deletePassage(long passageId) {
-        passageRepository.findById(passageId)
-                .orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
+	/**
+	 * 지문 삭제
+	 * 
+	 * @param passageId
+	 */
+	@Transactional
+	public void deletePassage(long passageId) {
+		passageRepository.findById(passageId).orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
 
-        passageRepository.deleteById(passageId);
-    }
+		passageRepository.deleteById(passageId);
+	}
 
-    /**
-     * 지문 수정을 누르면 보이는 화면
-     * 
-     * @param passageId
-     * @return
-     */
-    public SearchPassageDto searchPassageInfo(long passageId) {
-        SearchPassageDto searchPassageDto = new SearchPassageDto();
+	/**
+	 * 지문 수정을 누르면 보이는 화면
+	 * 
+	 * @param passageId
+	 * @return
+	 */
+	public SearchPassageDto searchPassageInfo(long passageId) {
+		SearchPassageDto searchPassageDto = new SearchPassageDto();
 
-        PassageEntity passage = passageRepository.findById(passageId)
-                .orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
+		PassageEntity passage = passageRepository.findById(passageId)
+				.orElseThrow(() -> new FlowerException(FlowerError.ENTITY_NOT_FOUND));
 
-        List<QuestionEntity> questionEntities = passage.getQuestionEntities();
+		List<QuestionEntity> questionEntities = passage.getQuestionEntities();
 
-        Map<String, List<SearchQuestionDto>> collect = questionEntities.stream()
-                .collect(Collectors.groupingBy(QuestionEntity::getQuestionCode,
-                        Collectors.mapping(q -> {
-                            SearchQuestionDto searchQuestionDto = new SearchQuestionDto();
-                            searchQuestionDto.setQuestionType(q.getQuestionType());
-                            searchQuestionDto.setQuestionId(q.getQuestionId());
+		List<SearchQuestionDtos> questions = new ArrayList<>();
 
-                            return searchQuestionDto;
-                        }, Collectors.toList())));
+		// code 별로 questionEntity grouping
+		Map<String, List<QuestionEntity>> groupByCode = questionEntities.stream()
+				.collect(Collectors.groupingBy(QuestionEntity::getQuestionCode));
 
-        searchPassageDto.setPassageContent(passage.getPassageContent());
-        searchPassageDto.setQuestions(collect);
+		for (Entry<String, List<QuestionEntity>> entry : groupByCode.entrySet()) {
+			String questionCode = entry.getKey();
+			List<QuestionEntity> questionByCode = entry.getValue();
 
-        return searchPassageDto;
-    }
+			SearchQuestionDtos searchQuestionDtos = new SearchQuestionDtos();
+			searchQuestionDtos.setQuestionCode(questionCode);
+			searchQuestionDtos.setQuestionTitle(questionByCode.get(0).getQuestionContentEntity().getQuestionTitle());
+			searchQuestionDtos.setQuestionContent(questionByCode.get(0).getQuestionContentEntity().getQuestionContent());
 
-    /**
-     * 지문 목록 조회
-     * 
-     * @param pageRequest
-     * @return
-     */
-    public Page<PassageListDto> searchPassageList(Pageable pageable, String passageYear,
-            String passageName) {
-        return passageRepository.findPassageAll(pageable, passageYear, passageName);
-    }
+			List<SearchQuestionDto> searchQuestionDto = new ArrayList<>();
+			for (QuestionEntity question : questionByCode) {
+				SearchQuestionDto questionDto = new SearchQuestionDto();
+				questionDto.setQuestionId(question.getQuestionId());
+				questionDto.setQuestionType(question.getQuestionType());
+				questionDto.setQuestionSubTitle(question.getQuestionSubTitle());
+				questionDto.setSubBox(question.getSubBox());
+				questionDto.setPastYn(question.isPastYn());
+				questionDto.setChoose(question.getChooseEntities().stream().map(choose -> {
+					ChooseDto chooseDto = new ChooseDto();
+					chooseDto.setSeq(choose.getChooseSeq());
+					chooseDto.setContent(choose.getChooseContent());
 
+					return chooseDto;
 
-    /**
-     * 교재 종류에 따라 현재 작성된 교재 이름이 포함된 교재 이름 목록 조회
-     * 
-     * @param passageType
-     * @param passageName
-     */
-    public List<PassageNameDto> searchPassageNameList(PassageType passageType, String passageName) {
-        return passageRepository.searchPassageNameList(passageType, passageName);
-    }
+				}).collect(Collectors.toList()));
+				questionDto.setAnswer(question.getAnswerEntities().stream().map(answer -> {
+					AnswerDto answerDto = new AnswerDto();
+					answerDto.setContent(answer.getAnswerContent());
 
-    public CheckExistPassageDto checkExistPassage(
-            PassageType passageType,
-            String passageYear,
-            String passageName,
-            String passageUnit,
-            String passageNumber) {
-        return passageRepository.checkExistPassage(passageType, passageYear, passageName, passageUnit, passageNumber);
-    }
+					return answerDto;
+				}).collect(Collectors.toList()));
 
-    private void setPassageEntity(PassageEntity passageEntity,
-            PassageRegistParam passageRegistParam) {
-        passageEntity.setPassageType(passageRegistParam.getPassageType());
-        passageEntity.setPassageYear(passageRegistParam.getPassageYear());
-        passageEntity.setPassageName(passageRegistParam.getPassageName());
-        passageEntity.setPassageUnit(passageRegistParam.getPassageUnit());
-        passageEntity.setPassageNumber(passageRegistParam.getPassageNumber());
-        passageEntity.setPassageContent(passageRegistParam.getPassageContent());
-    }
+				searchQuestionDto.add(questionDto);
+			}
+
+			searchQuestionDtos.setQuestion(searchQuestionDto);
+			questions.add(searchQuestionDtos);
+		}
+
+		searchPassageDto.setPassageContent(passage.getPassageContent());
+		searchPassageDto.setQuestions(questions);
+
+		return searchPassageDto;
+	}
+
+	/**
+	 * 지문 목록 조회
+	 * 
+	 * @param pageRequest
+	 * @return
+	 */
+	public Page<PassageListDto> searchPassageList(Pageable pageable, String passageYear, String passageName) {
+		return passageRepository.findPassageAll(pageable, passageYear, passageName);
+	}
+
+	/**
+	 * 교재 종류에 따라 현재 작성된 교재 이름이 포함된 교재 이름 목록 조회
+	 * 
+	 * @param passageType
+	 * @param passageName
+	 */
+	public List<PassageNameDto> searchPassageNameList(PassageType passageType, String passageName) {
+		return passageRepository.searchPassageNameList(passageType, passageName);
+	}
+
+	public CheckExistPassageDto checkExistPassage(PassageType passageType, String passageYear, String passageName,
+			String passageUnit, String passageNumber) {
+		return passageRepository.checkExistPassage(passageType, passageYear, passageName, passageUnit, passageNumber);
+	}
+
+	private void setPassageEntity(PassageEntity passageEntity, PassageRegistParam passageRegistParam) {
+		passageEntity.setPassageType(passageRegistParam.getPassageType());
+		passageEntity.setPassageYear(passageRegistParam.getPassageYear());
+		passageEntity.setPassageName(passageRegistParam.getPassageName());
+		passageEntity.setPassageUnit(passageRegistParam.getPassageUnit());
+		passageEntity.setPassageNumber(passageRegistParam.getPassageNumber());
+		passageEntity.setPassageContent(passageRegistParam.getPassageContent());
+	}
 }
