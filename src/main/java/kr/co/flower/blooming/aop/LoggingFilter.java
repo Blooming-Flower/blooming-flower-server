741136ce7092aa533c5b33135420a23d3d9f2c30
Min Hong <shmin7777@naver.com>
2023-09-25 18:59:16 +0900

    logging filter 추가

---
+++ src/main/java/kr/co/flower/blooming/aop/LoggingFilter.java
@@ -0,0 +1,80 @@
+package kr.co.flower.blooming.aop;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long start = System.currentTimeMillis();
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long end = System.currentTimeMillis();
+
+        log.debug("{} {} {} - {}ms\nHeaders : {}\nRequest : {}\nResponse : {}",
+                request.getMethod(),
+                request.getRequestURI(),
+                responseWrapper.getStatus(),
+                end - start,
+                getHeaders(request),
+                getRequestBody(requestWrapper),
+                getResponseBody(responseWrapper));
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper =
+                WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                return new String(buf);
+            }
+        }
+        return "-";
+    }
+
+    private String getResponseBody(final HttpServletResponse response) throws IOException {
+        String payload = null;
+        ContentCachingResponseWrapper wrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                payload = new String(buf);
+                wrapper.copyBodyToResponse();
+            }
+        }
+        return payload == null ? "-" : StringUtils.abbreviate(payload, 255);
+    }
+
+}
